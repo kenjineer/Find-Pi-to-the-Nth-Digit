907f45e7836e8ee61d0b7ea5f11c29b4fac12a15
kenjineer <kennethkaramihan@gmail.com>
2019-06-27 20:43:53 +0800

    Add Python solution

---
+++ find_pi.py
@@ -0,0 +1,21 @@
+from math import pi
+limit = True
+
+def Precision(num, place):
+    result = round(float(num), place)
+    if place == 0:
+        return int(result)
+    else:
+        return result
+
+while limit:
+    place = int(input("Enter how many decimal places of Pi to be displayed [LIMIT 15]: "))
+
+    if place < 16 and place >= 0:
+        limit = False
+        result = Precision(pi, place)
+    elif place > 15 or place < 0:
+        print("Decimal place out of bounds! Please enter a valid value.")
+        limit = True
+
+print("Pi in ", place, " decimal places is: ", result) 
