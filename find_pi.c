d56bb1a732bc88931e8aba00f21ea97a9c28fe44
Kenneth Caro Karamihan <kennethkaramihan@gmail.com>
2019-06-27 10:14:25 +0800

    Add files via upload

---
+++ find_pi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <float.h>
+
+int main(void)
+{
+    double pi = 4.0 * atan(1.0);
+    int place;
+    bool limit = true;
+
+    while (limit)
+    {
+        printf("Enter how many decimal places of Pi to be displayed [LIMIT 15]: ");
+        scanf("%i", &place);
+
+        if (place < 16 && place >= 0)
+            limit = false;
+        else if (place > 15 || place < 0)
+        {
+            printf("Decimal place out of bounds! Please enter a valid value.\n");
+            limit = true;
+        }
+    }
+    printf("Pi in %i decimal places is: %.*f", place, place, pi);
+}
